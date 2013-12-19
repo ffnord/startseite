62ec2f7e99422504000395752f233e49ef5379c1
Nils Schneider <nils@nilsschneider.net>
2013-12-19 20:38:34 +0100

    add nano-m to download page

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -9,6 +9,7 @@ HWREV_REGEX = /^(.+)-v(\d+)$/
 MODELMAP = {
   "ubnt-bullet-m" => { :make => "Ubiquiti", :model => "Bullet M" },
   "ubnt-unifi" => { :make => "Ubiquiti", :model => "UniFi AP" },
+  "ubnt-nano-m" => { :make => "Ubiquiti", :model => "Nanostation (Loco) M" },
 
   "tl-wdr3600"  => { :make => "TP-Link", :model => "TL-WDR3600" },
   "tl-wdr4300"  => { :make => "TP-Link", :model => "TL-WDR4300" },
