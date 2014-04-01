c68ad4d034bff1d4e3a5f0bbc8eb2cf093ced749
Nils Schneider <nils@nilsschneider.net>
2014-04-01 12:55:43 +0200

    firmware: update download list for 0.4

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -1,24 +1,32 @@
 require 'net/http'
 require 'uri'
 require 'nokogiri'
+require 'pp'
 
-FIRMWARE_REGEX = /^(\w+)-([\d.]+)-\w+-\w+-([\w-]+)-squashfs-factory.bin$/
-FIRMWARE_BASE = "http://metameute.de/~freifunk/firmware/0.3.2.2/"
+FIRMWARE_REGEX = /^gluon-((\w+)-([\d.]+)-([\w-]+)).bin$/
+FIRMWARE_BASE = "http://metameute.de/~freifunk/firmware/0.4/"
 HWREV_REGEX = /^(.+)-v(\d+)$/
 
 MODELMAP = {
-  "ubnt-bullet-m" => { :make => "Ubiquiti", :model => "Bullet M" },
-  "ubnt-unifi" => { :make => "Ubiquiti", :model => "UniFi AP" },
-  "ubnt-nano-m" => { :make => "Ubiquiti", :model => "Nanostation (Loco) M" },
+  "ubiquiti-bullet-m"        => { :make => "Ubiquiti", :model => "Bullet M, Nanostation Loco M" },
+  "ubiquiti-unifi"           => { :make => "Ubiquiti", :model => "UniFi AP (LR)" },
+  "ubiquiti-nanostation-m"   => { :make => "Ubiquiti", :model => "Nanostation M" },
+  "ubiquiti-unifiap-outdoor" => { :make => "Ubiquiti", :model => "UniFi AP Outdoor" },
 
-  "tl-wdr3600"  => { :make => "TP-Link", :model => "TL-WDR3600" },
-  "tl-wdr4300"  => { :make => "TP-Link", :model => "TL-WDR4300" },
-  "tl-wr1043nd" => { :make => "TP-Link", :model => "TL-WR1043ND" },
-  "tl-wr740n"   => { :make => "TP-Link", :model => "TL-WR741N" },
-  "tl-wr741nd"  => { :make => "TP-Link", :model => "TL-WR741ND" },
-  "tl-wr841n"   => { :make => "TP-Link", :model => "TL-WR841ND" },
-  "tl-wr841nd"  => { :make => "TP-Link", :model => "TL-WR841ND" },
-  "tl-wr842n"   => { :make => "TP-Link", :model => "TL-WR842N" }
+  "tp-link-tl-mr3020"      => { :make => "TP-Link", :model => "TL-MR3020" },
+  "tp-link-tl-mr3040"      => { :make => "TP-Link", :model => "TL-MR3040" },
+  "tp-link-tl-mr3220"      => { :make => "TP-Link", :model => "TL-MR3220" },
+  "tp-link-tl-mr3420"      => { :make => "TP-Link", :model => "TL-MR3420" },
+  "tp-link-tl-wdr3500"     => { :make => "TP-Link", :model => "TL-WDR3500" },
+  "tp-link-tl-wdr3600"     => { :make => "TP-Link", :model => "TL-WDR3600" },
+  "tp-link-tl-wdr4300"     => { :make => "TP-Link", :model => "TL-WDR4300" },
+  "tp-link-tl-wa901n-nd"   => { :make => "TP-Link", :model => "TL-WA901" },
+  "tp-link-tl-wr740n-nd"   => { :make => "TP-Link", :model => "TL-WR740" },
+  "tp-link-tl-wr741n-nd"   => { :make => "TP-Link", :model => "TL-WR741" },
+  "tp-link-tl-wr841n-nd"   => { :make => "TP-Link", :model => "TL-WR841" },
+  "tp-link-tl-wr842n-nd"   => { :make => "TP-Link", :model => "TL-WR842" },
+  "tp-link-tl-wr941n-nd"   => { :make => "TP-Link", :model => "TL-WR941" },
+  "tp-link-tl-wr1043n-nd"  => { :make => "TP-Link", :model => "TL-WR1043" },
 }
 
 module Jekyll
@@ -35,23 +43,26 @@ module Jekyll
   end
 
   class Firmware
-    attr_accessor :url
-    attr_accessor :filename
+    attr_accessor :basename
+    attr_accessor :factory
+    attr_accessor :sysupgrade
     attr_accessor :version
     attr_accessor :model
+    attr_accessor :make
     attr_accessor :hwrev
 
     def to_liquid
       {
-        "url" => url,
-        "filename" => filename,
+        "basename" => basename,
+        "factory" => factory,
+        "sysupgrade" => sysupgrade,
         "version" => version,
         "hwrev" => hwrev
       }
     end
 
     def to_s
-      self.filename
+      self.basename
     end
   end
 
@@ -66,24 +77,30 @@ module Jekyll
         end
       end
 
-      uri = URI.parse(FIRMWARE_BASE)
-      response = Net::HTTP.get_response uri
-      doc = Nokogiri::HTML(response.body)
-      links = doc.css('a')
-      hrefs = links.map do |link|
-        link.attribute('href').to_s
-      end.uniq.sort.select do |href|
-        href.match(FIRMWARE_REGEX)
+      def get_files(url)
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response uri
+        doc = Nokogiri::HTML(response.body)
+        doc.css('a').map do |link|
+          link.attribute('href').to_s
+        end.uniq.sort.select do |href|
+          href.match(FIRMWARE_REGEX)
+        end
       end
 
-      firmwares = hrefs.map do |href|
+      factory = get_files(FIRMWARE_BASE + "factory/")
+      sysupgrade = get_files(FIRMWARE_BASE + "sysupgrade/")
+
+      firmwares = Hash.new
+
+      factory.each do |href|
         fw = Firmware.new
-        fw.url = FIRMWARE_BASE + href
-        fw.filename = href
+        fw.factory = FIRMWARE_BASE + "factory/" + href
 
         href.match(FIRMWARE_REGEX) do |m|
-          fw.version = m[2]
-          fw.model = m[3]
+          fw.basename = m[1]
+          fw.version = m[3]
+          fw.model = m[4]
 
           fw.model.match(HWREV_REGEX) do |m|
             fw.model = m[1]
@@ -91,10 +108,22 @@ module Jekyll
           end
         end
 
-        fw
+        firmwares[fw.basename] = fw
       end
 
-      models = firmwares.group_by do |fw|
+      sysupgrade.each do |href|
+        path = FIRMWARE_BASE + "sysupgrade/" + href
+
+        href.match(FIRMWARE_REGEX) do |m|
+          basename = m[1].chomp "-sysupgrade"
+
+          if firmwares.has_key? basename
+            firmwares[basename].sysupgrade = path
+          end
+        end
+      end
+
+      models = firmwares.values.group_by do |fw|
         ModelDB.model(fw.model)
       end
 
